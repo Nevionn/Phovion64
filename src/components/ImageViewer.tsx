63be2968ca39ae3737325e1c369e47384055709c
Ancient <nevionn@yandex.ru>
2024-10-13 12:45:08 +0300

    добавлен компонент для просмотра фотографий

---
+++ src/components/ImageViewer.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
+
+const {width, height} = Dimensions.get('window');
+
+interface ImageViewerProps {
+  //   visible: boolean;
+  //   onClose: () => void;
+  //   imageSource: any;
+}
+
+const ImageViewer: React.FC<ImageViewerProps> = (
+  {
+    //   visible,
+    //   onClose,
+    //   imageSource,
+  },
+) => {
+  return (
+    <Modal
+      visible={true}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={() => {}}>
+      <View style={styles.modalContainer}>
+        <TouchableOpacity style={styles.closeButton} onPress={() => {}}>
+          <Text style={styles.closeText}>Close</Text>
+        </TouchableOpacity>
+
+        <View style={styles.imgView}>
+          <ReactNativeZoomableView
+            maxZoom={2.5}
+            minZoom={1}
+            zoomStep={0.5}
+            initialZoom={1}
+            bindToBorders={true}
+            style={styles.imgView}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/images/EHHttyOYx_Y.jpg')}
+            />
+          </ReactNativeZoomableView>
+          {/* <Image
+            source={require('../../assets/images/EHHttyOYx_Y.jpg')}
+            style={styles.image}
+            resizeMode="contain"
+          /> */}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default ImageViewer;
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.7)', // Полупрозрачный черный фон
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height * 1, // Окно для изображения
+    width: width * 1,
+    backgroundColor: 'black',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 40,
+    right: 20,
+    zIndex: 1,
+  },
+  closeText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -8,11 +8,18 @@ import {
   RefreshControl,
   ImageBackground,
   Dimensions,
+  Image,
 } from 'react-native';
 import {COLOR} from '../../assets/colorTheme';
 import NaviBar from '../components/Navibar';
 import Cbutton from '../components/Cbutton';
 import {usePinCodeRequest} from '../hooks/usePinCodeRequest';
+// import FastImage from 'react-native-fast-image';
+import ImageViewer from '../components/ImageViewer';
+import {ReactNativeZoomableView} from '@openspacelabs/react-native-zoomable-view';
+import {Image as SvgImage} from 'react-native-svg';
+const {width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 const MainPage = () => {
   const {showTableContent, dropTable} = usePinCodeRequest();
   return (
@@ -22,7 +29,9 @@ const MainPage = () => {
         translucent
         backgroundColor="transparent"
       />
-      <ScrollView contentContainerStyle={styles.root}>
+      {/* <ScrollView contentContainerStyle={styles.root}></ScrollView> */}
+      <View>{/* <ImageViewer /> */}</View>
+      <View style={styles.testBlock}>
         <Cbutton
           styleButton={{height: 40}}
           styleText={{}}
@@ -45,7 +54,7 @@ const MainPage = () => {
             dropTable('PinCodeTable');
           }}
         />
-      </ScrollView>
+      </View>
       <NaviBar />
     </View>
   );
@@ -59,9 +68,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  // photoItem: {
+  //   backgroundColor: 'black',
+  //   height: height * 0.5,
+  //   width: width * 0.75,
+  // },
+  // zoomableView: {
+  //   width: 300,
+  //   height: 300,
+  // },
+  // photo: {
+  //   width: '100%',
+  //   height: '100%',
+  // },
   text: {
     color: 'white',
     alignItems: 'center',
     fontSize: 18,
+  },
+  testBlock: {
+    flexDirection: 'row',
+    marginBottom: 100,
   },
 });
